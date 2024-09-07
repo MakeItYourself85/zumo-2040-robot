ea6db858960bf90a02639e6a5122891cfc5266f9
MakeItYourself85 <makron_04@yahoo.com>
2024-09-07 08:49:34 -0500

    Initial commit of remote_control.py

---
+++ micropython_demo/remote_control.py
@@ -0,0 +1,12 @@
+# This program will allow an operator to provide inputs via RC Transmitter
+# to control the robot
+
+from zumo_2040_robot import robot
+
+# Allow user to activate remote control mode and provide display to
+# confirm that RC Transmitter is connected and configured as expected
+
+# Display the interpreted receiver inputs
+
+
+
